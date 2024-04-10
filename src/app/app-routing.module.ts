5c55c28dff0b5143b63dd68e6c50b116c395c4c9
Kindra2804 <156190775+Kindra2804@users.noreply.github.com>
2024-04-10 20:12:03 +0200

    Navbar and App + Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'about', component:AboutComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: 'portfolio', component: PortfolioComponent
+  },
+  {
+    path: 'testimonials', component: TestimonialsComponent
+  },
+  {
+    path: 'hero', component: HeroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
