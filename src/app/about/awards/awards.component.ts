2a437ef6cae2f41f273d10ac4cd61f0bf39a5ac4
IlianaMarquez <iliana.marquez@hotmail.com>
2024-04-19 15:54:21 +0200

    added award tracking to change img display on hover

--- src/app/about/awards/awards.component.ts
+++ src/app/about/awards/awards.component.ts
@@ -11,12 +11,23 @@ export class AwardsComponent implements OnInit{
 
   printAwards!: iAward[];
   awards!: iAward;
-  
+  defaultImage= './assets/about_pics/award.svg';
+  hoverStates: boolean[]=[];
+  awardImage = '';
 
-  constructor (){}
+  constructor (){
+}
 
 ngOnInit(): void {
   this.printAwards = awards;
 }
+
+onMouseOver (index:number) {
+  this.hoverStates[index] = true;
+}
+
+onMouseOut(index:number){
+  this.hoverStates[index] = false;}
+
 
 }
--- src/app/awards.ts
+++ src/app/awards.ts
@@ -6,19 +6,19 @@ export const awards: iAward[]=[
         institution: 'International Photography Award',
         description: 'Winner of the prestigious International Photography Award for Best Portrait Series', 
         year: 2018,
-        image: 'award.svg',
+        image: '12.png',
     },
     {
         name: 'Excellence in Landscape Photography', 
         institution: 'Golden Lens Award',
         description: 'Recipient of the Golden Lens Award for Excellence in Landscape Photography', 
-        image: 'award.svg',
+        image: '2.png',
         year: 2020,
     },{
         name: 'Photographer of the Year', 
         institution: 'Vogue Paris',
         description: 'Named "Photographer of the Year" by Vogue Paris', 
-        image: 'award.svg',
+        image: '3.png',
         year: 2022,
     }
 ]
